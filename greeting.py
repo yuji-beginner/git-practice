a3a507ceb65d43b63ce3657af7b6473a750619a1
yuji-beginner <yousishengzhong@gmail.com>
2026-02-12 20:41:34 +0900

    feat: implement Greeting class

--- greeting.py
+++ greeting.py
@@ -1,2 +1,12 @@
-message = "Hello World!"
-print(message)
+class Greeting: 
+    message = ""
+
+    def __init__(self,greeting):
+        self.message = greeting
+
+    def greet(self):
+        print(self.message)
+    
+if __name__ == "__main__":
+    japanese = Greeting("こんにちは")
+    japanese.greet()
