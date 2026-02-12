3207035d4630cdd78805e92586e963e21ecf1c99
e235733 <e235733@ie.u-ryukyu.ac.jp>
2026-02-12 20:50:30 +0900

    feat: implement instance on main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from greeting import Greeting
+from burgerGenerator import BurgerGenerator
+
 print("this is main")
+
+gr = Greeting("Hello, World!")
+gr.greet()
+
+burgers = ["hamburger", "cheese burger", "chicken burger"]
+bg = BurgerGenerator(burgers)
+bg.generate()
